0fb364211b18abccf0a17148b2d7775278bb2481
riskaraini <riskaangraini2001@gmail.com>
2025-10-29 00:50:59 +0700

    Belajar mengenai tabel

--- resources/views/mahasiswa.php
+++ resources/views/mahasiswa.php
@@ -7,7 +7,40 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
 <body>
-<div class="container" style="margin-left: 30px"><h1>Ini adalah Halaman Mahasiswa</h1>
+<div class="container">
+<h1>Ini adalah Halaman Mahasiswa</h1>
+    <table class="table table-danger table-sm table-hover table-striped table-bordered text-center">
+        <thead>
+            <tr>
+                <th>NPM</th>
+                <th>Nama Mahasiswa</th>
+                <th>Jenis Kelamin</th>
+                <th colspan="2">TTL</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>123</td>
+                <td>Riska</td>
+                <td>Perempuan</td>
+                <td>20 Januari 2006</td>
+                <td rowspan="3">Binjai</td>
+            </tr>
+            <tr>
+                <td>111</td>
+                <td>Rai</td>
+                <td>Perempuan</td>
+                <td>20 Februari 2006</td>
+            </tr>
+            <tr>
+                <td>121</td>
+                <td>Aza</td>
+                <td>Perempuan</td>
+                <td>16 Oktober 2008</td>
+            </tr>
+        </tbody>
+    </table>
+
 </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
